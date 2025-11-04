e274eb12ba973ff83d32a043731fa178bbd1d2f3
abhiram <abhi18.b@gmail.com>
2025-11-04 23:38:55 +0530

    Revert "react fundamentals-react,reactDom using cdn"
    This reverts commit 42840e7f320f336e99fe262a61de49af4141d445.

--- basic/script.js
+++
@@ -1,6 +0,0 @@
-const root = document.getElementById("root");
-const h1 = document.createElement('h1');
-const textNode = document.createTextNode("Hello World");
-h1.appendChild(textNode)
-
-root.appendChild(h1)
