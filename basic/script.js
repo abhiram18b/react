42840e7f320f336e99fe262a61de49af4141d445
abhiram <abhi18.b@gmail.com>
2025-11-04 22:55:08 +0530

    react fundamentals-react,reactDom using cdn

---
+++ basic/script.js
@@ -0,0 +1,6 @@
+const root = document.getElementById("root");
+const h1 = document.createElement('h1');
+const textNode = document.createTextNode("Hello World");
+h1.appendChild(textNode)
+
+root.appendChild(h1)
