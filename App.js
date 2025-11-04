872a2b273910bbaea39d4012680b37cbe62bbbd7
abhiram <abhi18.b@gmail.com>
2025-11-04 23:52:17 +0530

    react fundamentals using react cdn

---
+++ App.js
@@ -0,0 +1,15 @@
+const parent = React.createElement("div",{id:"parent"},[
+        //1
+        React.createElement("div",{id:"child"},[
+            React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]),
+        //2
+        React.createElement("div",{id:"child-2"},[
+            React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")])
+    ]
+)
+const heading = React.createElement("h1",{id:"heading"},"Hello React")
+console.log(heading)
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(parent)
