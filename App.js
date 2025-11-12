95e4c6341022215cf6285a3e5af3ce2a8c877257
abhiram <abhi18.b@gmail.com>
2025-11-12 21:42:10 +0530

    initalized npm,installed react,readt-dom, parcel modules and removed cdn links

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
+import React from "react"
+import {createRoot} from "react-dom/client"
 const parent = React.createElement("div",{id:"parent"},[
         //1
-        React.createElement("div",{id:"child"},[
-            React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]),
+        React.createElement("div",{id:"child",key:"child"},[
+            React.createElement("h1",{key:"a1"},"I am h1 tag"),React.createElement("h2",{key:"a2"},"I am h2 tag")]),
         //2
-        React.createElement("div",{id:"child-2"},[
-            React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")])
+        React.createElement("div",{id:"child-2",key:"child-2"},[
+            React.createElement("h1",{key:"b1"},"I am h1 tag"),React.createElement("h2",{key:"b2"},"I am h2 tag")])
     ]
 )
 const heading = React.createElement("h1",{id:"heading"},"Hello React")
 console.log(heading)
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = createRoot(document.getElementById("root"))
 
 root.render(parent)
